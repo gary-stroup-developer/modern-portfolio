33fca42218ecf462a090d18442b504e7dcad7895
gary-stroup-developer <gstroup@Gary-Development-Station>
2024-05-21 19:52:10 -0700

    add bentobox layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+import { FaLocationArrow } from 'react-icons/fa'
 import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
@@ -25,9 +26,11 @@ const Hero = () => {
             <div className='flex justify-center relative my-20 z-10'>
                 <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
                     <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>Dynamic web portfolio</h2>
-                    <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words={'Transforming Concepts into Positive Experiences'} />
+                    <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl' words={'Transforming Concepts into Positive User Experiences'} />
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>Hi, I&apos;m Gary, a process engineer and data analyst</p>
-                    <MagicButton content={'View Projects'} />
+                    <a href="#about">
+                        <MagicButton title={'View Projects'} icon={<FaLocationArrow />} position='right' otherClasses='gap-2' />
+                    </a>
                 </div>
             </div>
         </div>
